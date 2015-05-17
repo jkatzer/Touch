6b47eb0202a6876bb90ab96fc5c3adf2b33b4a45
jason <json@katzr.com>
2015-05-17 18:16:15 -0400

    improved touch tracking event

--- Touch/UIResponder+TouchTracking.m
+++ Touch/UIResponder+TouchTracking.m
@@ -22,10 +22,52 @@
 - (void)xxx_touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
   [self xxx_touchesBegan:touches withEvent:event];
   NSLog(@"touchesBegan: %@", self);
-  [Touch addEvent:@{
-                    @"event":@"touchesBegan",
-                    @"object":[NSString stringWithFormat:@"%@", self]
-                    }];
+
+  for (UITouch* touch in touches) {
+    @try {
+      NSString* phase = @"";
+      switch (touch.phase) {
+        case UITouchPhaseBegan:
+          phase = @"UITouchPhaseBegan";
+          break;
+        case UITouchPhaseMoved:
+          phase = @"UITouchPhaseMoved";
+          break;
+        case UITouchPhaseStationary:
+          phase = @"UITouchPhaseStationary";
+          break;
+        case UITouchPhaseEnded:
+          phase = @"UITouchPhaseEnded";
+          break;
+        case UITouchPhaseCancelled:
+          phase = @"UITouchPhaseCancelled";
+          break;
+      }
+
+      [Touch addEvent:@{
+                        @"touch":@{
+                            @"phase":phase,
+                            @"view":@{
+                                @"class":[NSString stringWithFormat:@"%@", touch.view.class],
+                                @"frame":@[
+                                    [NSNumber numberWithFloat:touch.view.frame.origin.x],
+                                    [NSNumber numberWithFloat:touch.view.frame.origin.y],
+                                    [NSNumber numberWithFloat:touch.view.frame.size.height],
+                                    [NSNumber numberWithFloat:touch.view.frame.size.width]
+                                  ],
+                                @"tag":[NSNumber numberWithFloat:touch.view.tag],
+                                @"accessibilityLabel":[NSString stringWithFormat:@"%@", touch.view.accessibilityLabel],
+                                @"class":[NSString stringWithFormat:@"%@", touch.view.superview]
+                                }
+                            }
+                        }];
+    }
+    @catch (NSException *exception) {
+
+    }
+
+  }
+
 }
 
 @end
